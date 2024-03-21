b80106c318ba62d6fe49fb475b65505eb4aa9dd6
Пономарев Денис <desa-196@yandex.ru>
2024-03-21 16:39:48 +0300

    Добавил коментарии

--- Lesson_7/Program.cs
+++ Lesson_7/Program.cs
@@ -1,9 +1,13 @@
 ﻿using Lesson_7;
 
+//Создаем объект нашего тестового класса
 TestClass test = new TestClass(10, new char[] {'t', 'e', 's', 't'}, "Тестовая страка", 16.6m);
 
+//Преобразуем его в строку
 string resultString =  StringObject.ObjectToString(test) ;
 Console.WriteLine(resultString); //При выводе видим, что поле S класса TestClass cстало называться String
 
+//Преобразуем строку обратно в объект нашего тестового класса
 var objFromString = StringObject.StringToObject(StringObject.ObjectToString(test));
+//Смотрим, есть ли значение в поле с атрибутом
 Console.WriteLine("S = " + (objFromString as TestClass).S); //После обратного преобразования, видим, что в поле S теперь наше поле из строки под именем String
